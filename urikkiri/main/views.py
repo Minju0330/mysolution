7c69d228f6b26fa22e4e4a4a344a0a53125d9f37
“”Minju0330”” <alswnlee0330@gmail.com>
2020-07-23 16:04:40 +0900

    11111

--- urikkiri/main/views.py
+++ urikkiri/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Content
 
 # Create your views here.
+def home(request):
+    posts = Content.objects.all
+    return render(request, 'home.html', {'posts_list':posts})
